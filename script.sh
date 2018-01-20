0f09ce36a3cbc1cbfabe6381ccf4fe2cebd9d268
dani <dani@debian>
2018-01-20 12:07:24 +0100

    cambios

--- script.sh
+++ script.sh
@@ -12,13 +12,13 @@ for (( i=1 ; i<3 ; i++ )) ;do
 			;;
 	esac
 
-	
-		while [[ $estado == 'STOPPED' ]] ;do
-			lxc-start -n $host
-			estado= $(lxc-ls -f | grep $host | tr -s " " | cut -d " " -f 2)
+		
+	while [[ $estado == 'STOPPED' ]] ;do
+		lxc-start -n $host
+		estado= $(lxc-ls -f | grep $host | tr -s " " | cut -d " " -f 2)
 
 
-		done
+	done
 
 
 
