5157cfb8397155c63d7ab6520b2b38eca9bf2892
dani <dani@debian>
2018-01-20 13:01:11 +0100

    cambios

--- script.sh
+++ script.sh
@@ -18,9 +18,12 @@ for (( i=1 ; i<3 ; i++ )) ;do
 	done
 	lxc-device -n $host add /dev/mapper/BASCON-disco
 	lxc-attach -n $host-- mount /dev/mapper/BASCON-disco /var/www/html
-
+	ip= $(lxc-ls -f | grep maq1 | tr -s " " | cut -d " " -f 5)
+	if [[ $host == 'maq2' ]] ;then
+		iptables -t nat -D PREROUTING `iptables -t nat -L --line-number | egrep $ip | cut -d " " -f 1`
+	fi
+	iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $ip:80
 done
-
 
 
 
