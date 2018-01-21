76474b8dbb697f348648becd9b849965002379a8
dani <dani@debian>
2018-01-21 13:34:30 +0100

    acabado

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 for (( i=1 ; i<3 ; i++ )) ;do
+#definimos las dos máquinas, el tope de la ram y un comentario final
 	case $i in
 		1)
 			host='maq1'
@@ -14,6 +15,7 @@ for (( i=1 ; i<3 ; i++ )) ;do
 			var='maq1'
 			;;
 	esac
+#en esta parte desmontaremos el volumen, quitaremos la egla iptable y redimensionaremos el volumen
 	if [[ $host == 'maq2' ]] ;then
 		echo "Desmontamos el volumen de $var"
 		lxc-attach -n $var -- umount /dev/mapper/BASCON-disco		
@@ -28,11 +30,13 @@ for (( i=1 ; i<3 ; i++ )) ;do
 		sleep 2s
 		clear
 	fi
+#arrancamos la maquina
 	echo "arrancado $host"
 	while [ $(lxc-ls -f | grep $host | tr -s " " | cut -d " " -f 2) = 'STOPPED' ] ;do
 		lxc-start -n $host
 		ip=''
 	done
+#obtenimos la ip de la maquina	
 	while [ "$ip" !=  echo $(echo "$ip" | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}') ] ;do
 		echo 'obteniendo ip'
 		sleep 3s
@@ -40,21 +44,24 @@ for (( i=1 ; i<3 ; i++ )) ;do
 	done
 	clear
 	echo "ip obtenida: $ip"
-
+#añadimos el volumen logico y reiniciamos
 	echo 'añadiendo volumen logico'
 	lxc-device -n $host add /dev/mapper/BASCON-disco
 	lxc-attach -n $host -- mount /dev/mapper/BASCON-disco /var/www/html
 	lxc-attach -n $host -- systemctl restart apache2
+#añadimos la regla iptable
 	echo 'añadiendo regla iptable'
 	iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $ip:80
 	echo 'Momento de comprobación'
 	read 
+#control sobre el uso de memoria ram	
 	while [ $(lxc-attach -n $host -- free | grep Mem | tr -s " " | cut -d " " -f 4) -ge $num ] ;do
 		echo 'El consumo de RAM no ha superado el 70% de su capacidad'
 		sleep 3s
 	done
 	echo 'El consumo de RAM a susperado al 70%'
 	echo $comentario
+#aumento de la ram en vivo
 	if [[ $host == 'maq2' ]] ;then
 		lxc-cgroup -n $host memory.limit_in_bytes 2000M
 		echo 'Se ha amuntado la RAM'
@@ -64,7 +71,6 @@ for (( i=1 ; i<3 ; i++ )) ;do
 done
 iptables -t nat -D PREROUTING `iptables -t nat -L --line-number | egrep $ip | cut -d " " -f 1`
 lxc-stop -n $host
-
 
 #					stress -d 1 --vm-bytes 512M --timeout 10
 #					stress -d 1 --vm-bytes 1024 --timeout 10
